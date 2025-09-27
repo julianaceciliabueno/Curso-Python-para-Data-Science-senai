06dad7fff36ab76a5f0c4b5326134f2e76835ab9
Juliana Cecilia Bueno <61919008+julianaceciliabueno@users.noreply.github.com>
2025-09-27 16:17:42 -0300

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,12 @@
+#posicionamento: maria=0 idade=0,1 cidades=2 porto alegre =2,0, rio de janeiro =2,1
+lista = ["Maria",23, ["Porto Alegre","Rio de Janeiro"]]
+#print(lista.index("Porto Alegre"))
+print(lista[2][1])
+print(lista[2][0])
+print(lista[0])
+
+
+lista1 = ["do","re","mi"]
+lista2 = ["fa","sol","la","si"]
+lista = lista1 + lista2
+print(lista)
